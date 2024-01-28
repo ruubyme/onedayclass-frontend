12dadbf653ffc0b6f2a8e664bd22375c7b7b78a4
ruubyme <wngml1741@naver.com>
2024-01-28 11:11:49 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,7 +88,9 @@ const Main: React.FC<MainPageProps> = ({ initialAddress, initialClasses }) => {
   const [test, setTest] = useState<string>("");
 
   const testfunction = async () => {
-    const response = await flaskAPI.get("/print");
+    const response = await axios.get(
+      "https://port-0-flask-199u12dlrwtlmsu.sel5.cloudtype.app/print"
+    );
     setTest(response.data);
   };
 
