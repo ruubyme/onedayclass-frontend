6dfea206d99a4fa2bdbdcd06ff89747646ba479d
ruubyme <wngml1741@naver.com>
2024-02-08 09:32:01 +0900

    CSS: 선택한 tap css 적용

--- components/Tap.tsx
+++ components/Tap.tsx
@@ -13,7 +13,7 @@ const Tab: React.FC<TabProps> = ({ name, selectedTab, onSelect, children }) => {
       className={`
       px-4 py-1
        text-gray-600 
-      ${isActive ? " text-blue-700" : ""}
+      ${isActive ? "bg-gray-200 py-2 px-5" : ""}
       rounded-t 
       border-b-0 
       border-l border-r border-t 
